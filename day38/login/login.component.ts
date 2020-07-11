7c965b507b9bf2800e1c94d3a168e7b1b3b6d0a0
shefaly <shefalykasam@gmail.com>
2020-07-11 09:08:32 +0530

    day38

---
+++ day38/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+title="Login page render here";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
